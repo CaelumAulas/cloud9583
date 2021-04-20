4bdd16f62c250510e1db84015a8b6e5b07772b70
alefhsousa <alefh.sousa@gmail.com>
2021-04-20 20:52:10 -0300

    adiciona pagamento como um serviço

---
+++ fj33-eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.eats.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsPagamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatsPagamentoServiceApplication.class, args);
+	}
+
+}
