3941d30eae73249de4be4dfaa9771305eef187fe
alefhsousa <alefh.sousa@gmail.com>
2021-04-26 19:13:06 -0300

    client load balance no monolito

--- fj33-eats-monolito-modular/eats-application/src/main/java/br/com/caelum/eats/RestClientConfig.java
+++ fj33-eats-monolito-modular/eats-application/src/main/java/br/com/caelum/eats/RestClientConfig.java
@@ -1,5 +1,6 @@
 package br.com.caelum.eats;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,9 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 class RestClientConfig {
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
