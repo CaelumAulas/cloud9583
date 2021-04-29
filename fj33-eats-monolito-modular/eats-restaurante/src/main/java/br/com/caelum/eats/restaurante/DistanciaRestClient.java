55b21d201200848b2b7e562f3f29a9475961b5e6
alefhsousa <alefh.sousa@gmail.com>
2021-04-28 21:18:22 -0300

    adiciona comportamente de retry dentro do monolito

--- fj33-eats-monolito-modular/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/DistanciaRestClient.java
+++ fj33-eats-monolito-modular/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/DistanciaRestClient.java
@@ -3,10 +3,14 @@ package br.com.caelum.eats.restaurante;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 class DistanciaRestClient {
 
@@ -30,7 +34,10 @@ class DistanciaRestClient {
     }
   }
 
+  @Retryable(maxAttempts=5, backoff=@Backoff(delay=2000,multiplier=2))
   void restauranteAtualizado(Restaurante restaurante) {
+	log.info("monólito tentando chamar distancia-service");
+	  
     RestauranteParaServicoDeDistancia restauranteParaDistancia = new RestauranteParaServicoDeDistancia(restaurante);
     String url = distanciaServiceUrl+"/restaurantes/" + restaurante.getId();
     restTemplate.put(url, restauranteParaDistancia, RestauranteParaServicoDeDistancia.class);
